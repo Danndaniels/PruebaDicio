6846d3a2a60ec2fdd4b1e61afc3cb089b27d911a
Daniel Acosta <daniel.acosta.contacto@gmail.com>
2023-03-08 22:43:48 -0600

    add extension alert.

---
+++ PruebaDicio/Extension/Extension+ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Extension+ViewController.swift
+//  PruebaDicio
+//
+//  Created by Daniel Acosta on 08/03/23.
+//
+
+import UIKit
+extension UIViewController {
+  func alert(message: String, title: String = "") {
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    let OKAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+    alertController.addAction(OKAction)
+    self.present(alertController, animated: true, completion: nil)
+  }
+}
